2a64c202ebb7fefb7ec90e713ff072f4eca6a38f
Agustin Prieto <agustinprieto.ar@gmail.com>
2021-08-24 00:10:19 -0300

    added http query and gif display

--- src/app/gifs/services/gif.service.ts
+++ src/app/gifs/services/gif.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -6,10 +8,14 @@ import { Injectable } from '@angular/core';
 export class GifService {
 
   private _historial: string[] = [];
+  private apiKey: string = 'CwYRmyeGmrvBILuZDWPGxsrup1D1GLfv';
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
   }
+
+  constructor( private http: HttpClient ) {}
 
   buscarGifs( query: string){
     query = query.trim().toLowerCase();
@@ -18,6 +24,11 @@ export class GifService {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0,10);
     }    
-    console.log(this._historial);
+    
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=CwYRmyeGmrvBILuZDWPGxsrup1D1GLfv&q=${ query }&limit=10`)
+      .subscribe( ( resp ) => {
+        console.log( resp.data );
+        this.resultados = resp.data;
+      });
   }
 }
