d00f8ef7fa08489c84e9112fd1f2400716c15a13
Agustin Prieto <agustinprieto.ar@gmail.com>
2021-08-24 00:45:40 -0300

    charge images automatically

--- src/app/gifs/services/gif.service.ts
+++ src/app/gifs/services/gif.service.ts
@@ -8,27 +8,37 @@ import { SearchGifsResponse, Gif } from '../interfaces/gifs.interfaces';
 export class GifService {
 
   private _historial: string[] = [];
-  private apiKey: string = 'CwYRmyeGmrvBILuZDWPGxsrup1D1GLfv';
+  private apiKey: string = 'CwYRmyeGmrvBILuZDWPGxsrup1D1GLfv'; 
+
   public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
   }
 
-  constructor( private http: HttpClient ) {}
+  constructor( private http: HttpClient ) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+  }
 
   buscarGifs( query: string){
     query = query.trim().toLowerCase();
 
+    let apiLink: string = `https://api.giphy.com/v1/gifs/search?api_key=CwYRmyeGmrvBILuZDWPGxsrup1D1GLfv&q=${ query }&limit=10`;
+    
     if( !this._historial.includes( query ) ) {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+      // localStorage.setItem('resultados', JSON.stringify(this.))
     }    
     
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=CwYRmyeGmrvBILuZDWPGxsrup1D1GLfv&q=${ query }&limit=10`)
       .subscribe( ( resp ) => {
         console.log( resp.data );
         this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
       });
   }
 }
